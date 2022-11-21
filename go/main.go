e24880433b9e5b6e11f66fcb9d6db8e2210603a5
felipe gomes <fgss@cin.ufpe.br>
2022-11-21 01:26:05 -0300

    Probes (liveness, readiness), Resources, Metrics Server, HPA and Volumes (pvc)

--- go/main.go
+++ go/main.go
@@ -2,21 +2,28 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 type TestVersion struct {
 	Version string
 }
 
+var toggle bool = false
+
 func main() {
-	http.HandleFunc("/api", apiVersion)
+	http.HandleFunc("/apiVersion", ApiVersion)
+	http.HandleFunc("/health", Health)
+	http.HandleFunc("/toggle", Toggle)
+	go ToggleReady()
 	http.ListenAndServe(":8080", nil)
 }
 
-func apiVersion(rw http.ResponseWriter, req *http.Request) {
+func ApiVersion(rw http.ResponseWriter, req *http.Request) {
 
 	httpMethod := req.Method
 	env := os.Getenv("server-name")
@@ -30,4 +37,23 @@ func apiVersion(rw http.ResponseWriter, req *http.Request) {
 	}
 
 	json.NewEncoder(rw).Encode(versionResponse)
+}
+
+func Toggle(rw http.ResponseWriter, req *http.Request) {
+	toggle = !toggle
+	rw.WriteHeader(http.StatusOK)
+}
+func Health(rw http.ResponseWriter, req *http.Request) {
+	if toggle == true {
+		rw.WriteHeader(http.StatusOK)
+		return
+	}
+	rw.WriteHeader(http.StatusInternalServerError)
+
+}
+
+func ToggleReady() {
+	time.Sleep(time.Second * 10)
+	fmt.Println("Server ready! ")
+	toggle = true
 }
